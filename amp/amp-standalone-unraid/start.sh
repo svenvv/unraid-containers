34c6e79c7acbad6a2e1aa4f4db1c2790c6969c67
Richard Carruthers <richard@richardcarruthers.com>
2020-03-02 23:29:17 -0600

    fix script

--- amp/amp-standalone-unraid/start.sh
+++ amp/amp-standalone-unraid/start.sh
@@ -71,8 +71,8 @@ done
 if [ -z ${LICENCE+x} ]; then echo "Error: A licence for AMP from cubecoders.com is required and must be specified as --licence_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
 
 if [ ! -d ~/.ampdata/instances/$INSTANCE_NAME/ ]; then
-    echo "Creating module instance: ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}"
-    ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}
+    echo "Creating module instance: ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}"
+    ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}
     echo "Starting instance: $INSTANCE_NAME"
     (cd /ampdata/instances/$INSTANCE_NAME && exec ./AMP_Linux_x86_64)
 else
